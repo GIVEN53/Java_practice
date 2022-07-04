4261d150fc6607100628a152f870d2d721359cbb
Gibeom Nam <rlqja0523@naver.com>
2022-07-05 00:50:12 +0900

    constant first commit

---
+++ constant/src/Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+    public static final String IP_ADDRESS = "127.0.0.1";
+    public static final String SITE_NAME = "NAVER";
+}
---
+++ constant/src/codeTest.java
@@ -0,0 +1,9 @@
+public class codeTest {
+    public static void main(String[] args) {
+        String ipAddress = Constants.IP_ADDRESS;
+        String siteName = Constants.SITE_NAME;
+
+        System.out.println(ipAddress);
+        System.out.println(siteName);
+    }
+}
