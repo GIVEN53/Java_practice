fa598b77a2186ebb0c7ab0e8f132aa1cb6ee7e2d
Gibeom Nam <rlqja0523@naver.com>
2022-07-06 16:39:23 +0900

    Modify gitignore

--- constant/src/test.java
+++ constant/src/test.java
@@ -1,7 +1,7 @@
-public class codeTest {
+public class test {
     public static void main(String[] args) {
-        String ipAddress = Constants.IP_ADDRESS;
-        String siteName = Constants.SITE_NAME;
+        String ipAddress = constant.IP_ADDRESS;
+        String siteName = constant.SITE_NAME;
 
         System.out.println(ipAddress);
         System.out.println(siteName);
