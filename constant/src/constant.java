ef48ba44deee1bb8e19dc16096d0face151dac5d
Gibeom Nam <rlqja0523@naver.com>
2022-07-27 00:40:31 +0900

    Modify constant

--- constant/src/constant.java
+++ constant/src/constant.java
@@ -1,4 +1,4 @@
-public class constant {
+public class Constant {
     public static final String IP_ADDRESS = "127.0.0.1";
     public static final String SITE_NAME = "NAVER";
 }
--- constant/src/test.java
+++ constant/src/test.java
@@ -1,7 +1,7 @@
-public class test {
+public class Test {
     public static void main(String[] args) {
-        String ipAddress = constant.IP_ADDRESS;
-        String siteName = constant.SITE_NAME;
+        String ipAddress = Constant.IP_ADDRESS;
+        String siteName = Constant.SITE_NAME;
 
         System.out.println(ipAddress);
         System.out.println(siteName);
