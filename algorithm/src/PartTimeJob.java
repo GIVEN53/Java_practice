cd1c371a454abd750f8ee6a339aba6e231100f9a
Gibeom Nam <rlqja0523@naver.com>
2022-07-30 19:33:41 +0900

    Modify Algorithm PartTimeJob

--- algorithm/src/PartTimeJob.java
+++ algorithm/src/PartTimeJob.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class PartTimeJob {
     public static void main(String[] args) {
-        int ouput = Solution17.partTimeJob(4972);
-        System.out.println(ouput);
+        int output = Solution17.partTimeJob(4972);
+        System.out.println(output);
     }
 }
 
 class Solution17 {
-    public static int partTimeJob(int k) {
+/*    public static int partTimeJob(int k) {
         int[] coin = {500, 100, 50, 10, 5, 1}; // 가지고 있는 동전
         Queue<Integer> queue = new LinkedList<>();
 
@@ -23,5 +23,18 @@ class Solution17 {
             k = k%num;
         }
         return cnt;
+    }*/
+    public static int partTimeJob(int k) {
+        int[] dp = new int[k+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int[] coin = {1, 5, 10, 50, 100 ,500}; // 가지고 있는 동전
+
+        for (int i = 0; i < coin.length; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin[i]]+1);
+            }
+        }
+        return dp[k];
     }
 }
