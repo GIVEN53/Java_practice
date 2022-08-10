84eb672b733f1727f40dcada7766e709a7f0f4bd
Gibeom Nam <rlqja0523@naver.com>
2022-08-10 10:31:10 +0900

    Add algorithm 22.08.10

---
+++ algorithm/src/LargestProductOfThree.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class LargestProductOfThree {
+    public static void main(String[] args) {
+        int[] arr = {-1,2,-5,7};
+
+        Solution34 obj = new Solution34();
+        int output = obj.largestProductOfThree(arr);
+        System.out.println(output);
+    }
+}
+
+class Solution34 {
+    public int largestProductOfThree(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int[] temp = new int[3];
+
+        result = combination(0, 3, 0, arr, temp, result);
+
+        int max = Integer.MIN_VALUE;
+        for (int num : result) {
+            if (num > max) max = num; // 최대값 찾기
+        }
+        return max;
+    }
+
+    public ArrayList<Integer> combination(int n, int r, int start,int[] arr, int[] temp, ArrayList<Integer> result) { // 조합 메서드
+        if(n==r) {
+            return multiList(result, temp);
+        }
+        for (int i = start; i < arr.length; i++) {
+            temp[n] = arr[i];
+            int[] outcome = Arrays.copyOf(temp, temp.length);
+            result = combination(n+1, r, i+1, arr, outcome, result);
+        }
+        return  result;
+    }
+
+    public ArrayList<Integer> multiList(ArrayList<Integer> result, int[] temp) {
+        int sum = 1;
+        for (int num : temp) {
+            sum *= num; // 배열 요소의 곱
+        }
+        result.add(sum);
+        return result;
+    }
+}
